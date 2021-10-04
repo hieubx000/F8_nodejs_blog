d421f359f3f1eb88861d692cb8146216f799805d
Bui Xuan Hieu <buixuanhieutn@gmail.com>
2021-10-04 19:58:41 +0700

    17__

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars({
@@ -24,6 +29,16 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
     res.render('news');
+})
+
+app.get('/search', (req, res) => {
+    res.render('search');
+})
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+
+    res.send('');
 })
 
 app.listen(port, () => {
